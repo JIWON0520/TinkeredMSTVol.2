43830651b1286d14ce85a100ddcf74bc63999c90
JIWON0520 <jemajam12@gmail.com>
2021-12-08 17:35:07 +0900

    file update

--- InputProcessers/Terminal.java
+++ InputProcessers/Terminal.java
@@ -1,16 +1,21 @@
 package inputprocessers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Terminal {
 	private double xCoor;
 	private double yCoor;
 	private int numofPartition;
 	private Partition patition;
+	private List<Terminal> adjTerminal;
 	
 	public Terminal(double xCoor,double yCoor,int numOfPartition, Partition partition) {
 		this.xCoor=xCoor;
 		this.yCoor=yCoor;
 		this.numofPartition=numOfPartition;
 		this.patition=partition;
+		this.adjTerminal=new ArrayList<Terminal>();
 	}
 
 	public double getxCoor() {
@@ -29,10 +34,43 @@ public class Terminal {
 		return patition;
 	}
 	
+	public void setAdjTerminal(Terminal terminal) {
+		this.adjTerminal.add(terminal);
+	}
+	
 	@Override
 	public String toString() {
 		return "Termianl{"+
 				"("+xCoor+","+yCoor+") numOfPartition="+numofPartition+"}";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(xCoor);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(yCoor);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Terminal other = (Terminal) obj;
+		if (Double.doubleToLongBits(xCoor) != Double.doubleToLongBits(other.xCoor))
+			return false;
+		if (Double.doubleToLongBits(yCoor) != Double.doubleToLongBits(other.yCoor))
+			return false;
+		return true;
+	}
+
+	
 }
